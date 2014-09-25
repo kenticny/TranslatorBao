6d5f255e4721df4d6426fc334d3073d23d47cdaa
kenticny <kenticny@gmail.com>
2014-09-26 01:35:44 +0800

    achieved the basic translation function

--- assets/javascripts/background.js
+++ assets/javascripts/background.js
@@ -2,17 +2,15 @@
 function onClickHandler(info, tab) {
   if(info.menuItemId == "translate-selection") {
     var text = info.selectionText;
-    $.get("http://openapi.baidu.com/public/2.0/bmt/translate?client_id=z5OMaOk2GlfGj29UFP9siA6Y&q=today&from=auto&to=auto", 
+    $.get("http://openapi.baidu.com/public/2.0/bmt/translate?client_id=z5OMaOk2GlfGj29UFP9siA6Y&q=" + text + "&from=auto&to=auto", 
       function(result) {
-      //console.log(result);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {  
+          chrome.tabs.sendMessage(tabs[0].id, {message: result}, function(response) {    
+            console.log(response);
+          }); 
+        });
     });
   }
-
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {  
-    chrome.tabs.sendMessage(tabs[0].id, {message: "mycmd"}, function(response) {    
-      console.log(response);
-    }); 
-  });
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
--- assets/javascripts/contentscript.js
+++ assets/javascripts/contentscript.js
@@ -1,5 +1,14 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { 
-  console.log(request.message);
-  if(request.cmd== "mycmd") 
-    sendResponse("ok"); 
+  var showResult = "";
+  var transResult = request.message["trans_result"];
+  if(transResult.length == 0) {
+    showResult = "没有找到合适的解释";
+  }
+  for(var i = 0; i < transResult.length; i++) {
+    showResult += "<li>" + transResult[i].dst + "</li>";
+  }
+  if($("#translatebao").length > 0) {
+    $("#translatebao").remove();
+  }
+  $("body").append("<div id='translatebao' style='position:absolute;top:100px;right:100px;background:#000;color:#fff;padding:10px;'>" + showResult + "</div>");
 });

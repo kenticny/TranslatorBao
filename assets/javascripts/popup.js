017c8e6e6e9f8bf1cfb298b1ee098af3a8c9158b
kenticny <kenticny@gmail.com>
2014-10-04 00:29:01 +0800

    add popup translate

---
+++ assets/javascripts/popup.js
@@ -0,0 +1,24 @@
+$(function() {
+  var BG = chrome.extension.getBackgroundPage();
+
+  function parseResult(message) {
+    var transResult = message["trans_result"];
+    var showResult = "";
+    if(transResult.length == 0) {
+      showResult = "没有找到合适的解释";
+    }
+    for(var i = 0; i < transResult.length; i++) {
+      showResult += transResult[i].dst + "\n";
+    }
+    return showResult;
+  }
+
+  $("#transform>i").click(function() {
+    console.log(111)
+    var text = $("#from").val();
+    BG.translate(text, function(result) {
+      $("#to").val(parseResult(result))
+    });
+  });
+});
+

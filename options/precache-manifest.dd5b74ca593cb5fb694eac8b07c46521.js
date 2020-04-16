763b20569865eca6596e0eeaa561b46ca3919741
kenticny <kenticny@gmail.com>
2020-04-16 18:18:56 +0800

    options restructure

---
+++ options/precache-manifest.dd5b74ca593cb5fb694eac8b07c46521.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec19de13985385e2483cd619b3481fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "363ffe13dfec1ffd2568",
+    "url": "./static/css/main.a42bebb9.chunk.css"
+  },
+  {
+    "revision": "2c7192511c968670c233",
+    "url": "./static/js/2.14149ede.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.14149ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363ffe13dfec1ffd2568",
+    "url": "./static/js/main.0b168c0e.chunk.js"
+  },
+  {
+    "revision": "921aa36264cc2fb32342",
+    "url": "./static/js/runtime-main.a0cde188.js"
+  },
+  {
+    "revision": "18b70ba73e7b69aaa506e3bd7e2c25d2",
+    "url": "./static/media/iconfont.18b70ba7.woff"
+  },
+  {
+    "revision": "3ee9959ad8e8f781c596e1dfeb5007de",
+    "url": "./static/media/iconfont.3ee9959a.svg"
+  },
+  {
+    "revision": "d3257264647d5e2db2acde0cc33b29e1",
+    "url": "./static/media/iconfont.d3257264.eot"
+  },
+  {
+    "revision": "fd8659b4565ac1fb14832f1012c22421",
+    "url": "./static/media/iconfont.fd8659b4.ttf"
+  }
+]);

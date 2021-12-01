53b40a62ce3c19300b24cbabeacd31e00e552dec
kenticny <kenticny@gmail.com>
2021-12-02 00:05:49 +0800

    set property background

--- background/settings.js
+++ background/settings.js
@@ -22,6 +22,49 @@ class Settings {
       return {result: 'ok', data: res}
     })
   }
+  setProperty(obj) {
+    obj = obj ?? {}
+    return syncStorage.get('settings').then(res => {
+      return mergeDeep(res, obj)
+    }).then(res => {
+      return syncStorage.set({settings: res})
+    }).then(() => {
+      return {result: 'ok'}
+    })
+  }
+  getProperty(k) {
+    return syncStorage.get('settings').then(res => {
+      if (res) {
+        return res[k] || {}
+      }
+      return {result: 'ok',  data: {}}
+    }).then(res => {
+      return {result: 'ok', data: res}
+    })
+  }
 }
 
 var settings = new Settings()
+
+
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
